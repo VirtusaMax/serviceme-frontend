5c939e182d69a28cbf4598d78ab1d40ca50180d9
D.T.Thennakoon <154307023+dinithiZ@users.noreply.github.com>
2025-01-22 19:05:38 +0530

    update navbar

---
+++ src/components/custom-components/HowItWorks.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import backgroundImage from "../../assets/work_section/worksbg.png";
+import ReactPlayer from "react-player";
+
+const HowItWorks: React.FC = () => {
+  return (
+    <div
+      className="relative bg-cover bg-center py-20 px-4 sm:px-8 md:px-14 h-auto w-full top-4"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+      }}
+    >
+      <div className="absolute inset-0 bg-white bg-opacity-85"></div>
+
+      <div className="relative z-10 w-full">
+        <h2 className="text-yellow-400 text-3xl font-extrabold mb-8 text-left">
+          HOW IT WORKS?
+        </h2>
+
+        <div className="mx-auto max-w-4xl">
+          <div className="relative pb-[56.25%] h-0">
+            <ReactPlayer
+              url="https://youtu.be/0x5mf8BUJZY?si=xNZbFJSKlt9bKY4A"
+              className="absolute top-0 left-0 w-full h-full"
+              width="100%"
+              height="100%"
+              controls
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HowItWorks;
---
+++ src/components/custom-components/Navbar.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import {
+    Bars3BottomLeftIcon,
+    XMarkIcon,
+    UserCircleIcon,
+} from '@heroicons/react/24/solid'; // Import icons
+import logo from "../../assets/images/header_section/logo1.png";
+
+interface Link {
+    name: string;
+    link: string;
+}
+
+const Navbar: React.FC = () => {
+    const Links: Link[] = [
+        { name: 'Home', link: '/' },
+        { name: 'About', link: '/about' },
+        { name: 'Find Job', link: '/findjob' },
+        { name: 'Services', link: '/services' },
+    ];
+
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
+    return (
+        <div className='shadow-md w-full fixed top-0 left-0 border-b-4 border-white z-50'>
+            <div className='md:px-10 py-4 px-7 flex justify-between items-center bg-[#0E0524] h-[80px]'>
+                <div className='flex text-2xl items-center gap-2'>
+                    <img src={logo} alt="Logo" className="w-32 h-auto mt-8" />
+                </div>
+
+                <div
+                    onClick={() => setIsOpen(!isOpen)}
+                    className='w-7 h-7 absolute left-2 top-7 cursor-pointer md:hidden'
+                    style={{ color: 'white' }}
+                >
+                    {isOpen ? (
+                        <XMarkIcon style={{ color: 'white' }} />
+                    ) : (
+                        <Bars3BottomLeftIcon style={{ color: 'white' }} />
+                    )}
+                </div>
+
+                <ul
+                    className={`md:flex md:items-center md:ml-auto md:pb-0 pb-14 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-12 transition-all bg-[#0E0524] text-white duration-500 ease-in ${isOpen ? 'top-14' : 'top-[-490px]'}`}
+                    style={{ color: isOpen ? '' : 'white' }}
+                >
+                    {Links.map((link, index) => (
+                        <li key={index} className='font-inter font-semibold my-7 md:my-0 md:ml-8'>
+                            <a
+                                href={link.link}
+                                className="text-white hover:underline">
+                                {link.name}
+                            </a>
+                        </li>
+                    ))}
+
+                    <button className='btn bg-yellow-400 text-black py- px-3 md:ml-8 rounded md:static mr-4'>
+                        POST JOB
+                    </button>
+                </ul>
+
+                <div className="md:flex md:ml-0">
+                    <div className="absolute right-4 top-6 md:static md:right-auto">
+                        <UserCircleIcon
+                            className="w-10 h-10 text-white cursor-pointer hover:text-yellow-400"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
