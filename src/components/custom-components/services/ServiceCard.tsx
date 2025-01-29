3ee666b52c4fdd823c617f7572af89c5b2cda145
viduz <147256335+viduz19@users.noreply.github.com>
2025-01-29 09:09:39 -0400

    Complete FAQ page

--- src/components/custom-components/services/ServiceCard.tsx
+++ src/components/custom-components/services/ServiceCard.tsx
@@ -9,7 +9,8 @@ type ServiceCardProps = {
 
 const ServiceCard: React.FC<ServiceCardProps> = ({ name, img, img1, description }) => {
   return (
-    <div className="bg-[#0E0524] h-[350px] w-fit text-black rounded-xl shadow-lg flex flex-col p-4">
+    <div className="bg-[#0E0524] h-[350px] w-full text-black rounded-xl shadow-lg flex flex-col p-4">
+      {/* Main Image */}
       <div className="flex justify-center items-center w-full h-[175px]">
         <img
           src={img}
@@ -18,6 +19,7 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ name, img, img1, description 
         />
       </div>
 
+      {/* Icon */}
       <div className="relative flex justify-start mt-[-40px]">
         <div className="rounded-full ml-3">
           <img
@@ -28,9 +30,10 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ name, img, img1, description 
         </div>
       </div>
 
-      <div className="flex flex-col justify-center items-center gap-2 p-1 text-center">
+      {/* Description and Button */}
+      <div className="flex flex-col justify-center items-center gap-3 p-1 text-center">
         <p className="text-[12px] text-white font-regular m-0">{description}</p>
-        <button className="bg-[#FCC215] text-[#0E0524] text-xs h-[25px] px-6 py-0 rounded-xl mt-2">
+        <button className="bg-[#FCC215] text-[#0E0524] text-xs h-[30px] w-[120px] px-4 py-0 rounded-full mt-2">
           {name}
         </button>
       </div>
